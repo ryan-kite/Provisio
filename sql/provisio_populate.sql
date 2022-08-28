a06004ca6a3d39eaec12531e39466979f7105887
Ryan Kite <ryan.kite@zoro.com>
2022-08-28 12:59:37 -0700

    RLK Added mysql cheatsheet, schema, and populate script.

---
+++ sql/provisio_populate.sql
@@ -0,0 +1,43 @@
+//AMENITIES TABLE
+INSERT INTO amenities VALUES (' 1','30','WIFI')
+INSERT INTO amenities VALUES (' 2','45','POOL')
+INSERT INTO amenities VALUES (' 3','15','BREAKFAST')
+INSERT INTO amenities VALUES (' 4','50','PARKING')
+//AMENITIES CUSTOMER
+INSERT INTO customer VALUES(1,'jporter','jporter123','jennyporter@gmail.com','Jenny','Porter','200')
+INSERT INTO customer VALUES(2,'jsmith','jmith123','johnsmith@gmail.com','John','Smith','0')
+INSERT INTO customer VALUES(3,'jdow','jdow123','janesmith@gmail.com','Jane','Dow','0')
+//AMENITIES ATTRACTIONS
+INSERT INTO attractions VALUES (' 1','TEXAS','FISHING')
+INSERT INTO attractions VALUES (' 2','ARKANSAS','HORSE RIDING')
+INSERT INTO attractions VALUES (' 3','SEATTLE','HIKING')
+INSERT INTO attractions VALUES (' 4','COLORADO','SKIING')
+//AMENITIES HOTEL
+INSERT INTO hotel VALUES (' 4','TEXAS','FISHING')
+INSERT INTO hotel VALUES (' 5','ARKANSAS','HORSE RIDING')
+INSERT INTO hotel VALUES (' 6','SEATTLE','HIKING')
+//AMENITIES ROOMS
+INSERT INTO rooms VALUES (' 1','KING','1','350')
+INSERT INTO rooms VALUES (' 2','QUEEN','3','250')
+INSERT INTO rooms VALUES (' 3','DOUBLE','2','150')
+INSERT INTO rooms VALUES (' 4','DOUBLE','4','150')
+INSERT INTO rooms VALUES (' 5','KING','2','350')
+INSERT INTO rooms VALUES (' 6','QUEEN','1','250')
+//AMENITIES RESERVATION
+INSERT INTO reservations VALUES (' 1','1','4','1','2022-12-12','2022-12-20','1','1','1','150','380')
+INSERT INTO reservations VALUES (' 2','2','5','2','2022-12-12','2022-12-20','2','2','1','150','295')
+INSERT INTO reservations VALUES (' 3','3','6','3','2022-12-12','2022-12-20','3','3','1','150','165')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sql/provisio_schema.sql
@@ -0,0 +1,70 @@
+
+DROP DATABASE provisio;
+CREATE DATABASE provisio;
+USE provisio;
+
+CREATE TABLE Customer (
+  CustID int NOT NULL,
+  UserName varchar(255),
+  CustPass varchar(255),
+  EmailAddress varchar(255),
+  FirstName varchar(255),
+  LastName varchar(255),
+  LoyaltyPoints int NOT NULL,
+  PRIMARY KEY (CustID)
+);
+
+CREATE TABLE Attractions (
+  AttractionID int NOT NULL,
+  Location varchar(255),
+  AttractionDesc varchar(255),
+  PRIMARY KEY (AttractionID)
+);
+
+CREATE TABLE Amenities(
+  AmenityID int Not NULL,
+  AmenityPrice decimal(5,2),
+  AmenityDesc varchar(255),
+  PRIMARY KEY (AmenityID)
+); 
+
+CREATE TABLE Hotel (
+  HotelID int NOT NULL,
+  Location VARCHAR (255),
+  AttractionID INT NOT NULL,
+  PRIMARY KEY (HotelID),
+  FOREIGN KEY (AttractionID) REFERENCES Attractions(AttractionID)
+);
+
+CREATE TABLE Rooms (
+  RoomID int NOT NULL,
+  RoomType varchar(255),
+  RoomPrice decimal(5,2),
+  AmenityID int NOT NULL,
+  PRIMARY KEY (RoomID),
+  FOREIGN KEY (AmenityID) REFERENCES Amenities(AmenityID)
+);
+
+CREATE TABLE Reservation (
+  ReservationID int NOT NULL,
+  CustID int NOT NULL,
+  HotelID int NOT NULL,
+  RoomID int NOT NULL,
+  ChkInDate DATE NOT NULL,
+  ChkOutDate DATE NOT NULL,
+  Amenity_1 Char(255),
+  Amenity_2 Char(255),
+  Amenity_3 Char(255),
+  Amenity_4 Char(255),
+  Attraction_1 Char(255),
+  Attraction_2 Char(255),
+  Attraction_3 Char(255),
+  Attraction_4 Char(255),
+  TotalGuests int NOT NULL,
+  PointsEarned int NOT NULL,
+  TotalPrice decimal(5,2),
+  PRIMARY KEY (ReservationID),
+  FOREIGN KEY (HotelID) REFERENCES Hotel(HotelID),
+  FOREIGN KEY (CustID) REFERENCES Customer(CustID),
+  FOREIGN KEY (RoomID) REFERENCES Rooms(RoomID)
+);
